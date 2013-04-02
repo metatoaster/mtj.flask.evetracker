8cbe1c538c59462e49793df45bdf3d5d178d3cf7
Tommy Yu <y@metatoaster.com>
2013-04-03 00:04:11 +1300

    Import the whole module instead.
    - Using prefix for those is okay.

--- mtj/flask/evetracker/__init__.py
+++ mtj/flask/evetracker/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from flask import Blueprint, Flask, request, g, make_response, render_template
 
-from mtj.flask.evetracker.pos import overview, details
+from mtj.flask.evetracker import pos
 
 app = Flask('mtj.flask.evetracker')
 
@@ -14,5 +14,5 @@ def before_request():
 def teardown_request(exception):
     pass
 
-app.register_blueprint(overview, url_prefix='/overview')
-app.register_blueprint(details, url_prefix='/details')
+app.register_blueprint(pos.overview, url_prefix='/overview')
+app.register_blueprint(pos.tower, url_prefix='/tower')
