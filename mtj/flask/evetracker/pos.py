2e0d8f2bdcbbc2c02a35d376e5c50cd99d7757da
Tommy Yu <y@metatoaster.com>
2013-04-03 00:07:52 +1300

    Turns out route is more powerful than I thought.
    - So here are some renaming, too.

--- mtj/flask/evetracker/pos.py
+++ mtj/flask/evetracker/pos.py
@@ -6,6 +6,8 @@ from flask import Blueprint, Flask, request, g, make_response, render_template
 # template
 
 home = Blueprint('home', 'mtj.flask.evetracker.home')
+overview = Blueprint('overview', 'mtj.flask.evetracker.overview')
+tower = Blueprint('tower', 'mtj.flask.evetracker.tower')
 
 @home.route('/')
 def home_index():
@@ -14,27 +16,13 @@ def home_index():
     return response
 
 
-# Overview.
-
-overview = Blueprint('overview', 'mtj.flask.evetracker.overview')
-
-@overview.route('/')
-def overview_index():
-    result = render_template('overview.jinja')
-    response = make_response(result)
-    return response
-
-
-# Towers.
-
-tower = Blueprint('tower', 'mtj.flask.evetracker.tower')
-
 @tower.route('/')
-def tower_overview_index():
-    # TODO figure out how to share this with above overview?
-    result = render_template('overview.jinja')
+@overview.route('/')  # right now overview shares this.
+def overview_index():
+    result = render_template('tower_list.jinja')
     response = make_response(result)
     return response
+
 
 @tower.route('/<int:tower_id>')
 def tower_index(tower_id):
