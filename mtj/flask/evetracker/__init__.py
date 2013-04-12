9b96d533e22cf13992339e9084295fca7b3f167e
Tommy Yu <y@metatoaster.com>
2013-04-13 03:26:56 +1200

    Integrate with the flask module in mtj.eve.tracker
    - Also get that runner for free.

--- mtj/flask/evetracker/__init__.py
+++ mtj/flask/evetracker/__init__.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 
 from flask import Blueprint, Flask, request, g, make_response, render_template
 
+from mtj.eve.tracker.frontend.flask import json_frontend
+
 from mtj.flask.evetracker import util
 from mtj.flask.evetracker import pos
 
@@ -10,7 +12,17 @@ app = Flask('mtj.flask.evetracker')
 @app.before_request
 def before_request():
     g.navbar = app.config['MTJ_FLASK_NAV']
-    g.json_root = 'http://localhost:8000/json'
+
+    json_prefix = app.config.get('MTJPOSTRACKER_JSON_PREFIX', 'json')
+
+    # This is used by the javascript client.  Typically it is the same
+    # instance as this.
+    json_script_root = request.script_root
+    # Alternatively a different one may be specified, but must be
+    # accessible by the target end users.  This is for advanced usage.
+    # json_script_root = 'http://example.com/mtj.eve.tracker/frontend'
+
+    g.json_root = json_script_root + json_prefix
 
 @app.teardown_request
 def teardown_request(exception):
---
+++ mtj/flask/evetracker/ctrl.py
@@ -0,0 +1,5 @@
+from mtj.eve.tracker.ctrl import main
+from mtj.flask.evetracker import app
+
+if __name__ == "__main__":
+    main(app=app)
