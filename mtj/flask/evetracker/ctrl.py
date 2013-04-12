3b4d09ca42bea62f28507ce870a200bea91129bd
Tommy Yu <y@metatoaster.com>
2013-04-13 05:16:07 +1200

    Include custom options.

--- mtj/flask/evetracker/ctrl.py
+++ mtj/flask/evetracker/ctrl.py
@@ -1,5 +1,28 @@
-from mtj.eve.tracker.ctrl import main
+from __future__ import unicode_literals  # we are using json anyway.
+
+from mtj.eve.tracker.ctrl import Options, main
 from mtj.flask.evetracker import app
 
+
+class EvetrackerOptions(Options):
+
+    # Doing it the long way to not pollute the parent class.
+    default_config = {}
+    default_config.update(Options.default_config)
+    default_config.update({'users': {
+        'mode': 'default',
+        'setup_login': 'admin',
+        'setup_password': 'password',
+    }})
+
+    _schema = {}
+    _schema.update(Options._schema)
+    _schema.update({'users': {
+        'mode': ('default', 'setup', 'custom',),
+        'setup_login': basestring,
+        'setup_password': basestring,
+    }})
+
+
 if __name__ == "__main__":
-    main(app=app)
+    main(options=EvetrackerOptions(), app=app)
